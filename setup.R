13df593486ed8d8a390a00a4f5cf01d525e5171b
Peter Harrison <pmch2@cam.ac.uk>
2021-11-17 18:54:17 +0000

    Work in progress

--- setup.R
+++ setup.R
@@ -1,5 +1,13 @@
-embed_audio <- function(src, type = "audio/mpeg", attribute = "controls",
-                        placeholder = "") {
+library(glue)
+library(uuid)
+
+embed_audio <- function(
+  src, 
+  type = "audio/mpeg",
+  attribute = "controls",
+  placeholder = "",
+  ref = ""
+) {
   if (knitr::is_html_output()) {
     dir <- dirname(src)
     file <- basename(src)
@@ -9,11 +17,16 @@ embed_audio <- function(src, type = "audio/mpeg", attribute = "controls",
     R.utils::mkdirs(target_dir)
     file.copy(from = src, to = target_path, overwrite = TRUE)
     
-    cat(sprintf("<audio %3$s>
-               <source src='%1$s' type='%2$s'>
-            </audio>", src, type, attribute))
+    cat(sprintf("%4$s <audio %3$s><source src='%1$s' type='%2$s'></audio>", src, type, attribute, ref))
   } else cat(placeholder)
   
+}
+
+embed_image_with_audio <- function(image, audio, width, caption) {
+  ref <- UUIDgenerate()
+  cat(sprintf("![**%s** (ref:%s)]({%s}){width='%s'}", caption, ref, image, width))
+  cat("\n\n")
+  cat(sprintf("(ref:%s)", ref))
 }
 
 embed_video <- function(
